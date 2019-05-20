418edef260a971ccc5520fd73f56ba373e6a601d
yuuki <yuuki0xff@gmail.com>
2019-05-20 18:36:15 +0900

    add UnmarshalJSONInterface()

---
+++ cmdutils/example_json_test.go
@@ -0,0 +1,50 @@
+package cmdutils
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+type Foo struct {
+	Bar AbstractUnmarshaller
+}
+
+type Abstract interface{}
+type Concrete1 struct{ C1 string }
+type Concrete2 struct{ C2 string }
+
+type AbstractUnmarshaller struct {
+	Data Abstract
+}
+
+func (a *AbstractUnmarshaller) UnmarshalJSON(js []byte) error {
+	return UnmarshalJSONInterface(js, func(typeName string) (interface{}, error) {
+		switch typeName {
+		case "concrete1":
+			a.Data = &Concrete1{}
+		case "concrete2":
+			a.Data = &Concrete2{}
+		default:
+			return nil, errors.New("invalid type")
+		}
+		return a.Data, nil
+	})
+}
+
+func ExampleUnmarshalJSONInterface() {
+	var foo Foo
+
+	js := []byte(`{"bar": {"type": "concrete1", "c1": "ok"}}`)
+	err := json.Unmarshal(js, &foo)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(reflect.TypeOf(foo.Bar.Data))
+	fmt.Printf("%+v\n",foo.Bar.Data)
+	// Output:
+	// *cmdutils.Concrete1
+	// &{C1:ok}
+}
---
+++ cmdutils/json.go
@@ -0,0 +1,34 @@
+package cmdutils
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+// UnmarshalJSONInterface unmarshal the interface.
+//
+//   Input: {"type": "foo",  "extra-keys": "..."}
+//   Output: v = ConcreteType{...}
+//
+// TODO: improve docs
+func UnmarshalJSONInterface(js []byte, fn func(typeName string) (concrete interface{}, err error)) error {
+	var m map[string]interface{}
+	err := json.Unmarshal(js, &m)
+	if err != nil {
+		return err
+	}
+
+	typeName := m["type"]
+	if typeName == "" {
+		return errors.New(`missing "type" field`)
+	}
+	if t, ok := typeName.(string); !ok {
+		return errors.New(`"type" field is not string`)
+	} else {
+		concrete, err := fn(t)
+		if err != nil {
+			return err
+		}
+		return json.Unmarshal(js, concrete)
+	}
+}
