78aa50ca208c6ef66df68b433f80048f03b00391
yuuki <yuuki0xff@gmail.com>
2019-05-22 18:15:32 +0900

    add a test case for PveClient.buildUrl()

---
+++ provisioners/proxmoxve/api_test.go
@@ -0,0 +1,21 @@
+package proxmoxve
+
+import (
+	"github.com/magiconair/properties/assert"
+	"testing"
+)
+
+func TestPveClient_buildUrl(t *testing.T) {
+	t.Run("normal_case", func(t *testing.T) {
+		c := &PveClient{
+			Address: "base",
+		}
+		assert.Equal(t, "base/path", c.buildUrl("path"))
+	})
+	t.Run("slash", func(t *testing.T) {
+		c := &PveClient{
+			Address: "base/",
+		}
+		assert.Equal(t, "base/path", c.buildUrl("/path"))
+	})
+}
