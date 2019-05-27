a5f0db124cfa83de1a635dcb43aa4e490ae133f2
yuuki <yuuki0xff@gmail.com>
2019-05-27 14:29:17 +0900

    [WIP] impl PveProvisioner.Create()

--- provisioners/proxmoxve/provisioner.go
+++ provisioners/proxmoxve/provisioner.go
@@ -1,6 +1,7 @@
 package proxmoxve
 
 import (
+	"github.com/pkg/errors"
 	"github.com/yuuki0xff/clustertest/models"
 	"github.com/yuuki0xff/clustertest/provisioners"
 )
@@ -18,9 +19,58 @@ type PveProvisioner struct {
 	config models.InfraConfig // 具体的な型を入れる
 }
 
+// Create creates all resources of defined by PveSpec.
 func (p *PveProvisioner) Create() error {
-	// todo: get client
-	// todo: create resources
+	c := p.client()
+	err := c.Ticket()
+	if err != nil {
+		return errors.Wrap(err, "failed to get Proxmox VE API ticket")
+	}
+
+	// Create resources.
+	for _, vm := range p.spec.VMs {
+		from, err := c.IDFromName(vm.Template)
+		if err != nil {
+			return errors.Wrapf(err, "not found template: %s", vm.Template)
+		}
+		for i := 0; i < vm.Nodes; i++ {
+			// TODO: get an address from address pools.
+			ip := ""
+			// TODO: decide node.
+			nodeID := NodeID("")
+
+			toVMID, err := c.RandomVMID()
+			if err != nil {
+				return errors.Wrap(err, "failed to generate a random id")
+			}
+			to := NodeVMID{
+				NodeID: nodeID,
+				VMID:   toVMID,
+			}
+			err = c.CloneVM(from, to, "", "This VM created by clustertest-proxmox-ve-provisioner")
+			if err != nil {
+				return errors.Wrap(err, "failed to clone")
+			}
+			if vm.StorageSize > 0 {
+				err = c.ResizeVolume(to, "scsi0", vm.StorageSize)
+				if err != nil {
+					return errors.Wrap(err, "failed to resize")
+				}
+			}
+			err = c.UpdateConfig(to, &Config{
+				CPUCores:   vm.Processors,
+				CPUSockets: 1,
+				Memory:     vm.MemorySize * 1024,
+				User:       p.spec.User.User,
+				SSHKeys:    p.spec.User.SSHPublicKey,
+				IPAddress:  ip,
+			})
+			if err != nil {
+				return errors.Wrap(err, "failed to update config")
+			}
+		}
+	}
+
 	// todo: check resource status
 	// todo: update infra config
 	panic("not implemented")
