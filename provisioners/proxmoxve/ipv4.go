1e26883389f65ebfadeda2632ece714387fd64fc
yuuki <yuuki0xff@gmail.com>
2019-05-27 16:02:02 +0900

    impl IPv4Address

---
+++ provisioners/proxmoxve/ipv4.go
@@ -0,0 +1,104 @@
+package proxmoxve
+
+import (
+	"fmt"
+	"net"
+)
+
+type IPv4Address struct {
+	// IP address
+	ip net.IP
+	// Network address and mask.
+	network *net.IPNet
+	// String of the original CIDR notation IP address.
+	// This field is optional.
+	original string
+}
+
+func NewIPv4Address(cidrIP string) (*IPv4Address, error) {
+	ip, network, err := net.ParseCIDR(cidrIP)
+	if err != nil {
+		return nil, err
+	}
+	if ip.To4() == nil {
+		return nil, fmt.Errorf("invalid IPv4 address: %s", ip)
+	}
+	return &IPv4Address{
+		ip:       ip,
+		network:  network,
+		original: cidrIP,
+	}, nil
+}
+func newIPv4AddressByIP(ip net.IP, network *net.IPNet) *IPv4Address {
+	if !network.Contains(ip) {
+		err := fmt.Errorf("out of network ip address: %s is not contains %s", ip, network)
+		panic(err)
+	}
+	return &IPv4Address{
+		ip:      ip,
+		network: network,
+	}
+}
+func (a *IPv4Address) String() string {
+	ipnet := net.IPNet{
+		IP:   a.ip,
+		Mask: a.network.Mask,
+	}
+	return ipnet.String()
+}
+func (a *IPv4Address) Increase() (*IPv4Address, error) {
+	oldIPNumber := a.toUint32()
+	newIPNumber := oldIPNumber + 1
+
+	// check overflow
+	if oldIPNumber >= newIPNumber {
+		return nil, fmt.Errorf("out of range")
+	}
+
+	newIP := a.uint32ToIP(newIPNumber)
+	if a.network.Contains(newIP) {
+		return newIPv4AddressByIP(newIP, a.network), nil
+	}
+	return nil, fmt.Errorf("out of range")
+}
+
+// IsNetwork returns true if it represents network address.
+func (a *IPv4Address) IsNetwork() bool {
+	ip := a.toUint32()
+	bitmask := a.hostMask()
+	return ip&bitmask == 0
+}
+
+// IsBroadcast returns true if it represents broadcast address.
+func (a *IPv4Address) IsBroadcast() bool {
+	ip := a.toUint32()
+	bitmask := a.hostMask()
+	return ip&bitmask == bitmask
+}
+func (a *IPv4Address) toUint32() uint32 {
+	ipv4 := a.ip.To4()
+	if ipv4 == nil {
+		panic(fmt.Errorf("not ipv4: %s", a.ip.String()))
+	}
+
+	number := uint32(0)
+	for i := range ipv4 {
+		number = number<<8 + uint32(ipv4[i])
+	}
+	return number
+}
+func (a *IPv4Address) networkMask() uint32 {
+	return ^a.hostMask()
+}
+func (a *IPv4Address) hostMask() uint32 {
+	size, _ := a.network.Mask.Size()
+	return uint32(1)<<uint(size) - 1
+}
+func (*IPv4Address) uint32ToIP(n uint32) net.IP {
+	return net.IPv4(
+		byte(n>>24),
+		byte(n>>16),
+		byte(n>>8),
+		byte(n),
+	)
+}
---
+++ provisioners/proxmoxve/ipv4_test.go
@@ -0,0 +1,71 @@
+package proxmoxve
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewIPv4Address(t *testing.T) {
+	t.Run("should_success_when_valid_ip", func(t *testing.T) {
+		ip, err := NewIPv4Address("10.0.0.1/24")
+		assert.NoError(t, err)
+		assert.NotNil(t, ip)
+	})
+
+	t.Run("should_fail_when_invalid_ip", func(t *testing.T) {
+		ip, err := NewIPv4Address("12345")
+		assert.Error(t, err)
+		assert.Nil(t, ip)
+
+		ip, err = NewIPv4Address("10.0.0.0.0/24")
+		assert.Error(t, err)
+		assert.Nil(t, ip)
+
+		ip, err = NewIPv4Address("10.0.0/24")
+		assert.Error(t, err)
+		assert.Nil(t, ip)
+
+		ip, err = NewIPv4Address("10.0.0.300/24")
+		assert.Error(t, err)
+		assert.Nil(t, ip)
+
+		ip, err = NewIPv4Address("10.0.0.0")
+		assert.Error(t, err)
+		assert.Nil(t, ip)
+	})
+
+	t.Run("should_fail_when_ipv6", func(t *testing.T) {
+		ip, err := NewIPv4Address("::1/24")
+		assert.Error(t, err)
+		assert.Nil(t, ip)
+	})
+
+}
+func TestIPv4Address_Increase(t *testing.T) {
+	t.Run("should_success_when_next_ip_is_exists", func(t *testing.T) {
+		ip, err := NewIPv4Address("10.0.0.1/24")
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.NotNil(t, ip)
+
+		nextIP, err := ip.Increase()
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.NotNil(t, nextIP)
+		assert.Equal(t, "10.0.0.2/24", nextIP.String())
+	})
+
+	t.Run("should_fail_when_next_ip_is_not_exist", func(t *testing.T) {
+		ip, err := NewIPv4Address("10.0.0.255/24")
+		if !assert.NoError(t, err) {
+			return
+		}
+		assert.NotNil(t, ip)
+
+		nextIP, err := ip.Increase()
+		assert.EqualError(t, err, "out of range")
+		assert.Nil(t, nextIP)
+	})
+}
