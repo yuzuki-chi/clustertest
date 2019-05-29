03347e5d83b73c27a1dd516b281aabbeb76f0f4b
yuuki <yuuki0xff@gmail.com>
2019-05-29 17:32:03 +0900

    add test cases for Scheduler.Schedule()

--- provisioners/proxmoxve/scheduler_test.go
+++ provisioners/proxmoxve/scheduler_test.go
@@ -111,3 +111,92 @@ func TestScheduler_UpdateNodes(t *testing.T) {
 		assert.Equal(t, node, s.nodes[NodeID("1")])
 	})
 }
+
+func TestScheduler_Schedule(t *testing.T) {
+	t.Run("should_not_allocate_on_low_memory_node", func(t *testing.T) {
+		s := Scheduler{}
+		err := s.UpdateNodes(func() ([]*Node, error) {
+			return []*Node{
+				// Low memory (free memory is 500 MiB).
+				{
+					NodeID: NodeID("1"),
+					PMem:   1024,
+					VMem: struct {
+						System   int
+						Used     int
+						Reserved int
+					}{System: 24, Used: 500, Reserved: 0},
+				},
+			}, nil
+		}, false)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		_, err = s.Schedule(VMSpec{
+			Processors: 10,
+			Memory:     1000,
+		})
+		assert.Error(t, err)
+	})
+
+	t.Run("should_not_allocate_on_low_cpu_node", func(t *testing.T) {
+		s := Scheduler{}
+		err := s.UpdateNodes(func() ([]*Node, error) {
+			return []*Node{
+				// Low vCPUs (Number of free vCPUs is 8).
+				{
+					NodeID: NodeID("1"),
+					VCPU: struct {
+						Max      int
+						Used     int
+						Reserved int
+					}{Max: 16, Used: 8, Reserved: 0},
+					PMem: 2024,
+					VMem: struct {
+						System   int
+						Used     int
+						Reserved int
+					}{System: 24, Used: 500, Reserved: 0},
+				},
+			}, nil
+		}, false)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		_, err = s.Schedule(VMSpec{
+			Processors: 10,
+			Memory:     1000,
+		})
+		assert.Error(t, err)
+	})
+
+	t.Run("should_allocate_on_free_node", func(t *testing.T) {
+		s := Scheduler{}
+		err := s.UpdateNodes(func() ([]*Node, error) {
+			return []*Node{
+				// Free memory is 1000 MiB.
+				{
+					NodeID: NodeID("1"),
+					PMem:   2024,
+					VMem: struct {
+						System   int
+						Used     int
+						Reserved int
+					}{System: 24, Used: 500, Reserved: 0},
+				},
+			}, nil
+		}, false)
+		if !assert.NoError(t, err) {
+			return
+		}
+
+		id, err := s.Schedule(VMSpec{
+			Processors: 10,
+			Memory:     1000,
+		})
+		assert.NoError(t, err)
+		assert.Equal(t, NodeID("1"), id)
+	})
+}
