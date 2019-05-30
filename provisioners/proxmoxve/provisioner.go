d61af37a49836aff331b8a13ac4ee69c8fc2ea7d
yuuki <yuuki0xff@gmail.com>
2019-05-30 17:57:00 +0900

    add support for atomic resource allocation to PveProvisioner

--- provisioners/proxmoxve/provisioner.go
+++ provisioners/proxmoxve/provisioner.go
@@ -29,53 +29,74 @@ func (p *PveProvisioner) Create() error {
 		return errors.Wrap(err, "failed to get Proxmox VE API ticket")
 	}
 
+	err = p.updateSchedulerStatus(c)
+	if err != nil {
+		return errors.Wrap(err, "failed to update global scheduler status")
+	}
+
 	segs, err := p.segments()
 	if err != nil {
 		return err
 	}
 
 	// Create resources.
-	for _, vm := range p.spec.VMs {
-		from, err := c.IDFromName(vm.Template)
-		if err != nil {
-			return errors.Wrapf(err, "not found template: %s", vm.Template)
-		}
-		for i := 0; i < vm.Nodes; i++ {
-			// Get an address from global address pool.
-			ip := addresspool.GlobalPool.Allocate(segs)
-			// TODO: decide node.
-			nodeID := NodeID("")
-
-			toVMID, err := c.RandomVMID()
-			if err != nil {
-				return errors.Wrap(err, "failed to generate a random id")
-			}
-			to := NodeVMID{
-				NodeID: nodeID,
-				VMID:   toVMID,
-			}
-			err = c.CloneVM(from, to, "", "This VM created by clustertest-proxmox-ve-provisioner")
-			if err != nil {
-				return errors.Wrap(err, "failed to clone")
-			}
-			if vm.StorageSize > 0 {
-				err = c.ResizeVolume(to, "scsi0", vm.StorageSize)
+	err = GlobalScheduler.Transaction(func(scheduler *ScheduleTx) error {
+		return addresspool.GlobalPool.Transaction(func(pool *addresspool.AddressPoolTx) error {
+			for _, vm := range p.spec.VMs {
+				from, err := c.IDFromName(vm.Template)
 				if err != nil {
-					return errors.Wrap(err, "failed to resize")
+					return errors.Wrapf(err, "not found template: %s", vm.Template)
+				}
+				for i := 0; i < vm.Nodes; i++ {
+					// Allocate resources.
+					ip := pool.Allocate(segs)
+					nodeID, err := scheduler.Schedule(VMSpec{
+						Processors: vm.Processors,
+						Memory:     vm.MemorySize,
+					})
+					if err != nil {
+						return err
+					}
+
+					// Generate Random ID
+					toVMID, err := c.RandomVMID()
+					if err != nil {
+						return errors.Wrap(err, "failed to generate a random id")
+					}
+					to := NodeVMID{
+						NodeID: nodeID,
+						VMID:   toVMID,
+					}
+
+					// Clone specified VM and set up it.
+					err = c.CloneVM(from, to, "", "This VM created by clustertest-proxmox-ve-provisioner")
+					if err != nil {
+						return errors.Wrap(err, "failed to clone")
+					}
+					if vm.StorageSize > 0 {
+						err = c.ResizeVolume(to, "scsi0", vm.StorageSize)
+						if err != nil {
+							return errors.Wrap(err, "failed to resize")
+						}
+					}
+					err = c.UpdateConfig(to, &Config{
+						CPUCores:   vm.Processors,
+						CPUSockets: 1,
+						Memory:     vm.MemorySize * 1024,
+						User:       p.spec.User.User,
+						SSHKeys:    p.spec.User.SSHPublicKey,
+						IPAddress:  ip,
+					})
+					if err != nil {
+						return errors.Wrap(err, "failed to update config")
+					}
 				}
 			}
-			err = c.UpdateConfig(to, &Config{
-				CPUCores:   vm.Processors,
-				CPUSockets: 1,
-				Memory:     vm.MemorySize * 1024,
-				User:       p.spec.User.User,
-				SSHKeys:    p.spec.User.SSHPublicKey,
-				IPAddress:  ip,
-			})
-			if err != nil {
-				return errors.Wrap(err, "failed to update config")
-			}
-		}
+			return nil
+		})
+	})
+	if err != nil {
+		return err
 	}
 
 	// todo: check resource status
@@ -141,4 +162,43 @@ func (p *PveProvisioner) segments() ([]addresspool.Segment, error) {
 		})
 	}
 	return segs, nil
+}
+func (p *PveProvisioner) updateSchedulerStatus(c *PveClient) error {
+	return GlobalScheduler.UpdateNodes(func() ([]*Node, error) {
+		nodeInfos, err := c.ListNodes()
+		if err != nil {
+			return nil, err
+		}
+
+		var nodes []*Node
+		for _, n := range nodeInfos {
+			var totalCPUs int
+			var totalMem int
+			vms, err := c.ListVMs(n.ID)
+			if err != nil {
+				return nil, err
+			}
+			for _, vm := range vms {
+				totalCPUs += vm.Cpus
+				totalMem += vm.Mem
+			}
+
+			nodes = append(nodes, &Node{
+				NodeID: n.ID,
+				PCPU:   n.MaxCPU,
+				VCPU: struct {
+					Max      int
+					Used     int
+					Reserved int
+				}{Max: 0, Used: totalCPUs, Reserved: 0},
+				PMem: n.MaxMem,
+				VMem: struct {
+					System   int
+					Used     int
+					Reserved int
+				}{System: DEFAULT_SYSTEM_MEM, Used: totalMem, Reserved: 0},
+			})
+		}
+		return nodes, nil
+	}, false)
 }
