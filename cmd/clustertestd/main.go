8703ef7d1e22f3d3d8d2643b9ee36a4674876820
yuuki <yuuki0xff@gmail.com>
2019-06-06 19:27:29 +0900

    add commands on clustertestd command

--- cmd/clustertestd/main.go
+++ cmd/clustertestd/main.go
@@ -8,12 +8,40 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:              "clustertestd",
-	Short:            "Clustertest server",
+	Short:            "Start clustertest daemon",
 	TraverseChildren: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// TODO
 		panic("not implemented")
 	},
+}
+var statusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "Show current daemon status",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// TODO
+		panic("not implemented")
+	},
+}
+var stopCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "daemon going to graceful shutdown",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// TODO
+		panic("not implemented")
+	},
+}
+var killCmd = &cobra.Command{
+	Use:   "kill",
+	Short: "Stop daemon immediately",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// TODO
+		panic("not implemented")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(statusCmd, stopCmd, killCmd)
 }
 
 func main() {
