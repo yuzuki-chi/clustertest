857bf64db02bc81514d348099a8ec3343eaafc4a
yuuki <yuuki0xff@gmail.com>
2019-06-10 02:05:29 +0900

    impl "clustertest task start" command

--- cmd/clustertest/task_start_cmd.go
+++ cmd/clustertest/task_start_cmd.go
@@ -1,19 +1,61 @@
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/yuuki0xff/clustertest/cmdutils"
+	. "github.com/yuuki0xff/clustertest/cmdutils"
+	"github.com/yuuki0xff/clustertest/models"
+	"github.com/yuuki0xff/clustertest/rpc"
+	"io/ioutil"
 )
 
 func taskStartFn(cmd *cobra.Command, args []string) error {
-	confs, err := loadConfigs(args)
+	c, err := rpc.NewClient()
 	if err != nil {
-		cmdutils.ShowError(err)
+		ShowError(err)
 		return nil
 	}
 
-	for _, conf := range confs {
-		_ = conf
-		// TODO: enqueue conf
+	files, err := findConfigs(args)
+	if err != nil {
+		ShowError(err)
+		return nil
 	}
+
+	for _, file := range files {
+		task, err := newTaskFromFile(file)
+		if err != nil {
+			ShowError(err)
+			return nil
+		}
+
+		id, err := c.Create(task)
+		if err != nil {
+			ShowError(err)
+			return nil
+		}
+		fmt.Println(id)
+	}
+	return nil
+}
+
+func newTaskFromFile(name string) (models.Task, error) {
+	data, err := ioutil.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileTask{name, data}, nil
+}
+
+type FileTask struct {
+	name string
+	data []byte
+}
+
+func (t *FileTask) String() string {
+	return t.name
+}
+func (t *FileTask) SpecData() []byte {
+	return t.data
 }
